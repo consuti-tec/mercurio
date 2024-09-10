bffd8bdcdcf3d3f6079697bf5b91ab018b613fb5
consuti-tec <consuti@consuti.com.br>
2024-09-09 21:25:59 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#90292a",
+  UrlPortal: "https://plataforma.sporti.com.br/mercurioesportes",
+  UrlPainel: "https://painel.sporti.com.br/",
+  TimeSplashScreen: 3000,
+};
